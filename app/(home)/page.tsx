e5ebf5be237716596ee272e57d972f73fc7950ff
Mayza <mayzamrodrigues@gmail.com>
2024-01-31 18:22:32 -0300

    feat: add welcome mensage

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import { ptBR } from 'date-fns/locale'
 import Header from '../_components/header'
+import {format} from "date-fns"
 
 function Home() {
   return (
     <div>
         <Header/>
+        <div className='px-5 pt-5'>
+        <h2 className='text-xl font-bold'>Ola Miguel</h2>
+        <p className='capitalize text-sm'>{format(new Date (), "EEEE', 'dd 'de' yyyy", {
+            locale:ptBR,
+        })}</p>
+        </div>
+
     </div>
+
+    
   )
 }
 
