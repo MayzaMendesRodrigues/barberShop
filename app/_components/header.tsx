1c0612b9b3c80eef41e877c0c3ed6c715de19d7d
Mayza <mayzamrodrigues@gmail.com>
2024-02-06 00:10:56 -0300

    feat: add AuthProvider

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,16 +1,32 @@
+"use client"
 import { MenuIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import { Card, CardContent } from "./ui/card";
 import Image from "next/image";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const Header = () => {
+    const {data} = useSession()
+    const handleLoginClick = async () => {
+        console.log('login was clicked')
+        await signIn()
+    }
     return ( 
         <Card>
             <CardContent className="px-5 py-8 justify-between items-center flex flex-row">
             <Image src="/logo.png" alt="FSW Barber" height={22} width={130}></Image>
-            <Button variant="outline" size="icon" className="h-8 w-8">
-                <MenuIcon size={16}/>
-            </Button>
+            
+            {data?.user ? (
+            <div>
+                <Button onClick={() => signOut}>Logout</Button>
+                <h1>{data.user.name}</h1>
+            </div> ): (
+            <Button variant="outline" size="icon" className="h-8 w-8" onClick={handleLoginClick}>
+            <MenuIcon size={16}/>
+        </Button>
+
+
+            )}
 
             </CardContent>
         </Card>
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,12 +2,13 @@ import { db } from "@/app/_lib/prisma"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import NextAuth from "next-auth"
 import { Adapter } from "next-auth/adapters"
-import GoogleProvider from 'next-auth/providers/google'
+import GoogleProvider from "next-auth/providers/google"
 
 const handler = NextAuth({
     adapter: PrismaAdapter(db) as Adapter,
-    providers:[GoogleProvider({
-        clientId:process.env.GOOGLE_CLIENT_ID as string,
+    providers:[
+        GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID as string,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
 ],
